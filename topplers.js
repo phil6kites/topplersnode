29230537cb18373a59ffaf255bc8cb2f60b493e1
Phil Maddaloni <phil@6kites.com>
2014-01-24 16:36:51 -0700

    paypal ipn

--- topplers.js
+++ topplers.js
@@ -1,13 +1,26 @@
 var express = require('express');
 var logfmt = require('logfmt');
 var app = express();
+var https = require('https');
 
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 
-	res.type('text/plain');
-	res.send('Welcome To Topplers');
+	res.type('text/html');
+
+	var the_page = '<html><head><title>Topplers Test Node App</title></head><body><h2>Welcome To Topplers</h2>';
+	
+	the_page += '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">';
+	the_page += '<input type="hidden" name="cmd" value="_s-xclick">';
+	the_page += '<input type="hidden" name="hosted_button_id" value="UCXW9MR7EPBGQ">';
+	the_page += '<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">';
+	the_page += '<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">';
+	the_page += '</form></body></html>';
+
+	res.send(the_page);
+
+	res.end();
 });
 
 app.get('/topplers/test', function(req, res) {
@@ -19,6 +32,11 @@ app.get('/topplers/test', function(req, res) {
 	res.json(json_test);
 });
 
+app.post('/paypal/ipn', function(req, res) {
+
+	console.log(req);
+});
+
 app.post('/orders/new', function(req, res) {
 
 	console.log(req);
@@ -27,6 +45,55 @@ app.post('/orders/new', function(req, res) {
 
 	res.json(json_test);
 });
+
+app.get('/topplers/orders', function(req, res) {
+
+	console.log(req);
+
+	res.type('text/plain');
+	res.send("thanks, done");
+
+	var options = {
+		hostname: 'topplers-dev-shop.myshopify.com',
+		port: 443,
+		path: '/admin/orders.json',
+		method: 'GET',
+		auth: '0c7d701cf12b04bfc9db01cc2f9111ee:9288007fd4c55695fe95d4c07229e863'
+	};
+
+
+	var req = https.request(options, function(response) {
+
+		//console.log('status = ', response.statusCode);
+		//console.log('headers = ', response.headers);
+
+		response.on('data', function(d) {
+
+			process.stdout.write(d);
+			//process.stdout.write('\n------\n');
+			//process.stdout.write(d.orders);
+			//res.send(d);
+			//console.log(d);
+
+			//displayOrders(d);
+		});
+
+	});
+
+	req.end();
+
+	req.on('error', function(e) {
+
+		console.error(e);
+	});
+
+	console.log("orders remote request done");
+});
+
+function displayOrders(orders) {
+
+	console.log("Found " + orders.length + " orders returned from shopify")
+}
 
 /*
 function by_name(a, b) {
